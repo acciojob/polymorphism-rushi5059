c9804fe14422385f8da9442ef1941e71e1c90779
Rushikesh Pote <rushi5059@gmail.com>
2023-04-24 17:18:08 +0530

    Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans1 = p.product(10, 20);
+        int ans2 = p.product(10, 20, 30);
+        double ans3 = p.product(10, 20);
+    }
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 }
